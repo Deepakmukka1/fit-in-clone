f779e77968a69a952a64ec2a5c99c9bb4eaf00b3
Mukka Deepak <ramu92013@gmail.com>
2023-08-25 15:34:24 +0530

    🎉 init: initialize boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LandingPage from './pages/landing.page'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-blue-900'>
+        <LandingPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
