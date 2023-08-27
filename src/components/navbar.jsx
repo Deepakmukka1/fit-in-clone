0a37bb49ddc9fb6c8331e31acec78e3d0a1df87e
Mukka Deepak <ramu92013@gmail.com>
2023-08-27 18:52:59 +0530

    🩹 fix: replace svg with icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Logo from "../assets/logo3.png";
 
+import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const NavigationBar = () => {
-  const [state, setState] = useState(false);
+  const [isNavClicked, setNavClicked] = useState(false);
   const navigation = [
     { title: "How it works", path: "#howto" },
     { title: "Plans", path: "#plans" },
@@ -20,57 +23,39 @@ const NavigationBar = () => {
           <div className="lg:hidden">
             <button
               className="text-textprimary outline-none p-2 rounded-md"
-              onClick={() => setState(!state)}
+              onClick={() => setNavClicked(!isNavClicked)}
             >
-              {state ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+              {isNavClicked ? (
+                <FontAwesomeIcon icon={faXmark} />
               ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 8h16M4 16h16"
-                  />
-                </svg>
+                <FontAwesomeIcon icon={faBars} />
               )}
             </button>
           </div>
         </div>
         <div
           className={`flex-1 justify-center text-base text-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
-            state ? "h-screen flex flex-col items-center" : "hidden h-auto"
+            isNavClicked ? "h-screen flex flex-col items-center" : "hidden"
           }`}
         >
-          <ul className={`justify-center md:justify-end items-center space-y-4 lg:flex lg:space-x-6 lg:space-y-0`}>
+          <ul
+            className={`justify-center md:justify-end items-center space-y-4 lg:flex lg:space-x-6 lg:space-y-0`}
+          >
             {navigation.map((item, idx) => {
               return (
                 <li
                   key={idx}
                   className="text-textprimary font-medium pr-4 hover:text-gray-800 md:text-lg transition-all tracking-wider"
+                  onClick={() => setNavClicked(!isNavClicked)}
                 >
                   <a href={item.path}>{item.title}</a>
                 </li>
               );
             })}
-            <button className="bg-[#343A40] text-white rounded-md px-4 py-2 font-medium hover:bg-[#495057] transition-all duration-3000">
+            <button
+              className="bg-[#343A40] text-white rounded-md px-4 py-2 font-medium hover:bg-[#495057] transition-all duration-3000"
+              onClick={() => setNavClicked(!isNavClicked)}
+            >
               <a href="#contact"> Free trail </a>
             </button>
           </ul>
