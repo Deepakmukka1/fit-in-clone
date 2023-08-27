cd2ba68334fd77950bc17259cc03dd48381157a3
Mukka Deepak <ramu92013@gmail.com>
2023-08-27 16:53:40 +0530

    ✨ feat: add contact section

---
+++ src/components/contactform.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Contact1 from "../assets/contact1.jpeg";
+const ContactForm = () => {
+  return (
+    <div className="grid lg:grid-cols-3 grid-cols-1 bg-gradient-to-r from-[#9b9ea1] to-[#dee2e6]  max-w-screen-xl mx-auto w-4/5 lg:w-full text-[#555555] rounded-xl">
+      <div className="col-span-2 lg:px-20 sm:px-10 sm:py-16 p-6 flex flex-col gap-5 justify-center">
+        <h2 className="md:text-5xl text-xl font-semibold">Get one week free trial!</h2>
+        <h3 className="md:text-lg text-sm">
+          Get fit and eat healthy with us. Start your fitness journey today. You
+          can pause or cancel anytime. And the first week trial is on us!
+        </h3>
+        <form className="grid lg:grid-cols-2 grid-cols-1 gap-8 items-end text-base">
+          <div className="block">
+            <label htmlFor="fullname" className="block mb-2">Full Name</label>
+            <input
+              type="text"
+              placeholder="Your name"
+              name="fullname"
+              className="md:p-3 px-2 py-1 bg-[#dee2e6] rounded-lg shadow-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="block mb-2 ">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="you@example.com"
+              className="md:p-3 px-2 py-1 text-base bg-[#dee2e6] rounded-lg shadow-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="where" className="block mb-2 font-base">Where did you hear about us?</label>
+            <select name="where" className="md:p-3 px-2 py-1 text-base bg-[#dee2e6] rounded-lg shadow-lg w-full">
+              <option value="">Please choose any one:</option>
+              <option value="friends">Friends or Family</option>
+              <option value="socials">Social Media</option>
+              <option value="newspaper">Newspaper</option>
+              <option value="tv">Television AD</option>
+            </select>
+          </div>
+
+            <button className="bg-[#9EA1A4] w-full p-3 rounded-xl text-black hover:bg-white transition-all duration-700">Sign up now</button>
+        </form>
+      </div>
+      <div className="-order-1 lg:order-1">
+        <img src={Contact1} className="object-cover w-full rounded-t-xl lg:rounded-r-xl h-96 lg:h-full" />
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/contactsection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactForm from './contactform'
+
+const ContactSection = () => {
+  return (
+    <section className='mx-auto max-w-screen-xl pt-32 pb-32 flex justify-center lg:px-10 px-2' id='contact'>
+        <ContactForm/>
+    </section>
+  )
+}
+
+export default ContactSection
